8974671ae04b7ad35c3a61064773ee9ec6b2edec
JonnyRivers <jonny.w.rivers@gmail.com>
2021-11-26 16:22:40 -0800

    Basic event printing

--- src/Testbed/OpenDataClient.cs
+++ src/Testbed/OpenDataClient.cs
@@ -52,6 +52,67 @@ namespace Testbed
         private void LoadEvents(string path)
         {
             string json = File.ReadAllText(path);
+            IEnumerable<Dictionary<string, JsonElement>> records = JsonSerializer.Deserialize<IEnumerable<Dictionary<string, JsonElement>>>(json);
+            foreach (var record in records)
+            {
+                IdNamePair eventType = record["type"].Deserialize<IdNamePair>();
+
+                switch (eventType.name)
+                {
+                    case "Starting XI":
+                        ProcessStartingEleven(record);
+                        break;
+                    case "Half Start":
+                        ProcessHalfStart(record);
+                        break;
+                    case "Pass":
+                        ProcessPass(record);
+                        break;
+                    case "Ball Receipt*":
+                        ProcessBallReceipt(record);
+                        break;
+                    case "Carry":
+                        ProcessCarry(record);
+                        break;
+                    default:
+                        throw new Exception("unknown event");
+                }
+            }
+        }
+
+        private void ProcessStartingEleven(Dictionary<string, JsonElement> elementsByName)
+        {
+            IdNamePair team = elementsByName["team"].Deserialize<IdNamePair>();
+            Console.WriteLine($"Starting eleven for {team.name}");
+        }
+
+        private void ProcessHalfStart(Dictionary<string, JsonElement> elementsByName)
+        {
+            IdNamePair team = elementsByName["team"].Deserialize<IdNamePair>();
+            Console.WriteLine($"Half starting for {team.name}");
+        }
+
+        private void ProcessPass(Dictionary<string, JsonElement> elementsByName)
+        {
+            IdNamePair player = elementsByName["player"].Deserialize<IdNamePair>();
+            Dictionary<string, JsonElement> pass = elementsByName["pass"].Deserialize<Dictionary<string, JsonElement>>();
+            IdNamePair recipient = pass["recipient"].Deserialize<IdNamePair>();
+
+            Console.WriteLine($"Pass from {player.name} intended for {recipient.name}");
+        }
+
+        private void ProcessBallReceipt(Dictionary<string, JsonElement> elementsByName)
+        {
+            IdNamePair player = elementsByName["player"].Deserialize<IdNamePair>();
+
+            Console.WriteLine($"Pass received by {player.name}");
+        }
+
+        private void ProcessCarry(Dictionary<string, JsonElement> elementsByName)
+        {
+            IdNamePair player = elementsByName["player"].Deserialize<IdNamePair>();
+
+            Console.WriteLine($"Ball carried by {player.name}");
         }
     }
 }
